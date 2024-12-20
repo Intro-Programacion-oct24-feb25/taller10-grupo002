1421d284241af2ca1e84e1bf114952b7304ddf4c
Javier Vinan <javinan5@utpl.edu.ec>
2024-12-19 20:45:53 -0500

    problema06

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -10,5 +10,32 @@ package paquete6;
  * @author reroes
  */
 public class Problema06 {
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        
+        String acumuladora = "";
+        
+        for(int fila = 0; fila < estudiantes.length; fila++)
+            for(int col = 0; col < estudiantes[fila].length; col++){
+                if(estudiantes[fila][col].length() == 11){
+                    acumuladora = String.format("%s%s\n",
+                            acumuladora,
+                            estudiantes[fila][col]);
+            }
+        }
+        
+        System.out.printf("Los nombres con 11 caracteres son: \n%s",
+                acumuladora);
+        
+        
+        
+        
+        
+        
+    }
     
 }
